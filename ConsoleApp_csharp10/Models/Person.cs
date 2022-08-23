ebb2c9543875c0c1a0b8635cc42dca88fb3c9b8f
Karen Payne <paynekaren@comcast.net>
2022-08-23 08:11:11 -0700

    C# 10 mix syntax

---
+++ ConsoleApp_csharp10/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp_csharp10.Models
+{
+    public class Person
+    {
+        public int Id { get; init; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        public override string ToString() => $"{Id} {FullName}";
+
+        public void Deconstruct(out string firstName, out string lastName) =>
+            (firstName, lastName) = (FirstName, LastName);
+
+    }
+}
---
+++ ConsoleApp_csharp10/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp_csharp10.Models;
+
+namespace ConsoleApp_csharp10
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                FirstName = "Karen",
+                LastName = "Payne"
+            };
+
+            string lastname = "Does not matter :-)";
+            (string firstname, lastname) = person;
+
+            AnsiConsole.MarkupLine($"[b]Hello[/] [yellow]{firstname}[/] [cyan]{lastname}[/]");
+            Console.ReadLine();
+        }
+    }
+}
